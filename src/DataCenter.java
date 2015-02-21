806d3e603166d19f8ed58434ba7e7e72bf452d57
Haitong Tian <htian3@siebl-0222-06.ews.illinois.edu>
2015-02-21 15:52:37 -0600

    build connection between servers

--- src/DataCenter.java
+++ src/DataCenter.java
@@ -3,14 +3,38 @@ import java.net.*;
 import java.util.*;                                              
 
 public class DataCenter{
-	public static List<Packet> message_queue;
-	public static Map<Integer, Socket> socket_map; 
+	private static Queue<Packet> message_queue;
+	private static Socket socket_map[] = new Socket[4]; 
+	private static int ports[] = new int[4];
+	private static String host_name = new String("127.0.0.1");
 
-	public static int ports[] = new int[4];
+	public static synchronized Packet getMessage(){
+	// Return one message if the message queue is not empty
+		return message_queue.poll();
+	}
+
+	//@Parameter: machine ID
+	public static Socket getSocket(Integer id){
+	// Return one message if the message queue is not empty
+		return socket_map[id];
+	}
+
+	//@Parameter: current machine ID
+	public static void buildConnection(int id) throws IOException{
+		for(int i=0; i < 4; i++){
+		// Build socket with machines which have bigger IDs than current machine
+			if(i >= id){
+				Socket server_socket = new Socket(host_name,ports[i]);
+				socket_map[i] = server_socket;
+			}
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
-//		Client client = a.
-//		a.run();
-
+    	if (args.length != 2) {
+        	System.err.println("Usage: java KKMultiServer <port number>");
+	        System.exit(1);
+    	}
 	}
 
 }
