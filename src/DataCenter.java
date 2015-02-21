702503a159a508f4779dbeac567aef24a5c32261
Haitong Tian <htian3@siebl-0222-06.ews.illinois.edu>
2015-02-21 14:39:28 -0600

    Message skeleton

---
+++ src/DataCenter.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;                                              
+
+public class DataCenter{
+	public static List<Packet> message_queue;
+	public static Map<Integer, Socket> socket_map; 
+
+	public static int ports[] = new int[4];
+	public static void main(String[] args) throws IOException {
+//		Client client = a.
+//		a.run();
+
+	}
+
+}
---
+++ src/Packet.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;                                              
+import java.net.*;
+
+public class Packet{
+	public String content;
+	public int type;
+}
