359d70a65496af37f056a862ad1b700432d43850
Mengjia Yan <mengjiayan0720@gmail.com>
2015-02-28 19:50:20 -0600

    add content.java

---
+++ src/Content.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class Content implements Serializable{
+    public int value;
+    public long timestamp;
+	public Content(int v, long t){
+		value = v;
+		timestamp = t;
+	}
+
+}
+
+
