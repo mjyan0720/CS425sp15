be52d6e9e42668e173802ffb5539e3f7dce338f3
Haitong Tian <tianhaitong@gmail.com>
2015-02-28 18:35:39 -0600

    Enabled update

--- src/Packet.java
+++ src/Packet.java
@@ -30,7 +30,8 @@ public class Packet implements Serializable{
         Ack
     };
 
-
+	public Packet(){
+	}
     //initialize Packet with its content, creation time and delay
     public Packet(String s, long t){
         this.content = s;
